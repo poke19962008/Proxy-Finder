e6f78168dd309e43f264ea20ffce769f695635e5
Sayan Das <poke19962008@gmail.com>
2015-09-02 11:53:46 +0530

    Shows all the IP addresses open.

--- proxy.sh
+++ proxy.sh
@@ -1,19 +1,20 @@
 #AUTHOR: poke19962008
 
 rm -f log.txt	#Remove Previous Log File
-fin="0"
+fin=( "0" )
 
 for i in {0..100}
 do
 	prxy="172.16.0.$i:8080"
 	echo "Connecting: $prxy"
-	curl --proxy $prxy --max-time 3 --silent 74.125.224.72 --output "log.txt"
+	curl --proxy $prxy --max-time 1 --silent 74.125.224.72 --output "log.txt"
 	
 	if [[ -s log.txt ]]
 	then
 		echo "STATUS: Success"   #Received Data
-		fin="$prxy"
-		break
+		fin=( ${fin[@]} $prxy )
+		rm -f log.txt
+		echo " "
 	else
 		echo "STATUS: Failed\n"  #Not Received Data
 	fi
@@ -21,9 +22,11 @@ done
 
 echo "---------------------------------------------------------------"
 
-if [[ $fin != "0" ]]
+unset fin[0]
+if [[ ${#fin[@]} -ne 0 ]]
 then
-	echo "Open Proxy Server: "$fin
+	echo ${#fi[@]}" Proxy IP are open."
+	echo ${fi[@]}
 else
 	echo "Sorry no proxy server is open."
 fi
